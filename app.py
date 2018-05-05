a7304e4e50bf47dcfd436877c3cb954d2810510d
SinghSourav <krsourav7@gmail.com>
2018-05-05 19:31:46 +0530

    Here is first

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+import os
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+app = Flask(__name__)
+'''
+@app.route("/")
+def index():
+    return "Flask App!"
+@app.route("/hello")
+def hello1():
+    return "this is hello you write string in '/string'"
+#@app.route("/hello/<string:name>")
+@app.route("/hello/<string:name>/")
+def hello(name):
+#    return name
+    quotes = [ "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
+               "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
+               "'To understand recursion you must first understand recursion..' -- Unknown",
+               "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown",
+               "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
+               "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"  ]
+    randomNumber = randint(0,len(quotes)-1)
+    quote = quotes[randomNumber]
+
+    return render_template(
+        'test.html',**locals())
+'''
+
+@app.route('/')
+def home():
+
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('aboutus.html')
+        #" <a href='/logout'>Logout</a>"
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+#normal login placeholder
+'''
+    if request.form['password']=='password' and request.form['username']=='admin':
+        session['logged_in']=True
+    else:
+        flash("wrong password!!")
+    return home()
+    '''
+
+
+#logout root
+@app.route("/logout")
+def logout():
+    session['logged_in']=False
+    return home()
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
